48d15927a06fc9791e7a72566f7115e44f2faaaa
Tim Hunt <T.J.Hunt@open.ac.uk>
2016-08-03 19:20:03 +0100

    Bump version number for the 3.2 release

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016011800;
-$plugin->requires  = 2014041100;
+$plugin->version   = 2016080300;
+$plugin->requires  = 2015111600;
 $plugin->component = 'report_customsql';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.1 for Moodle 3.0+';
+$plugin->release   = '3.2 for Moodle 3.0+';
 
 $plugin->outestssufficient = true;
