f61982310e0f6c3ff2f5fda39dfe7bd019e83703
hieuvu <hieu.vuvan@harveynash.vn>
2018-06-25 15:59:54 +0700

    GDPR: report_customsql #262803 #267511

--- lang/en/report_customsql.php
+++ lang/en/report_customsql.php
@@ -147,3 +147,4 @@ $string['verifyqueryandupdate'] = 'Verify the Query SQL text and update the form
 $string['weeklyheader'] = 'Weekly';
 $string['weeklyheader_help'] = 'These queries are automatically run on the first day of each week, to report on the previous week. These links let you view the results that has already been accumulated.';
 $string['whocanaccess'] = 'Who can access this query';
+$string['privacy:metadata'] = 'The Ad-hoc database queries plugin does not store any personal data.';
