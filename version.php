a1bead9ae022c799a4c66a32fa1abf0071ccffdf
Tim Hunt <T.J.Hunt@open.ac.uk>
2018-11-22 15:51:54 +0000

    Test Moodle 3.3 compatibility

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018080900;
-$plugin->requires  = 2017111300;
+$plugin->requires  = 2017051500;
 $plugin->component = 'report_customsql';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.5 for Moodle 3.4+';
+$plugin->release   = '3.5 for Moodle 3.3+';
 
 $plugin->outestssufficient = true;
