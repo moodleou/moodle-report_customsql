882afea3e1d765de4722debc511fcb901d16a227
jnlar <jonarcher@catalyst-au.net>
2022-12-21 14:30:17 +1100

    Fix #123: Fix schema issues in report_customsql_categories

--- tests/local/category_test.php
+++ tests/local/category_test.php
@@ -28,7 +28,7 @@ require_once($CFG->dirroot . '/report/customsql/locallib.php');
  * @copyright 2021 The Open University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class _category_test extends \advanced_testcase {
+class category_test extends \advanced_testcase {
     /**
      * Test create category.
      */
