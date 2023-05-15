177c3d78c73f86f4bd36d8da7330ca1ff97256c2
hieuvu <hieuvu@nashtechglobal.com>
2023-05-15 13:16:31 +0700

    Adhoc-reports: Fix PHP 8.1 deprecated error for str_replace(), reset(), explode() #679596

--- edit_form.php
+++ edit_form.php
@@ -158,7 +158,7 @@ class report_customsql_edit_form extends moodleform {
     public function set_data($currentvalues) {
         global $DB, $OUTPUT;
 
-        $currentvalues->emailto = explode(',', $currentvalues->emailto);
+        $currentvalues->emailto = explode(',', $currentvalues->emailto ?? '');
         parent::set_data($currentvalues);
 
         // Add report information.
--- locallib.php
+++ locallib.php
@@ -507,7 +507,7 @@ function report_customsql_write_csv_row($handle, $data) {
     global $CFG;
     $escapeddata = array();
     foreach ($data as $value) {
-        $value = str_replace('%%WWWROOT%%', $CFG->wwwroot, $value);
+        $value = str_replace('%%WWWROOT%%', $CFG->wwwroot, $value ?? '');
         $value = str_replace('%%Q%%', '?', $value);
         $value = str_replace('%%C%%', ':', $value);
         $value = str_replace('%%S%%', ';', $value);
