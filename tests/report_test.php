993b1bd51e3d87ce9e0c39c11cf470b3eb74263e
Oscar Nadjar <oscar.nadjar@moodle.com>
2024-10-02 12:14:22 -0500

    Reverting doc changes and pipeline errors fix

--- tests/report_test.php
+++ tests/report_test.php
@@ -97,11 +97,6 @@ class report_test extends \advanced_testcase {
         $this->assertEquals($expected, report_customsql_get_week_starts(strtotime($datestr)));
     }
 
-
-    /**
-     * Test plugin get_month_starts method.
-     * @covers ::report_customsql_get_month_starts
-     */
     public function test_get_month_starts_test(): void {
         $this->assertEquals([
                 strtotime('00:00 1 November 2009'), strtotime('00:00 1 October 2009')],
@@ -116,10 +111,6 @@ class report_test extends \advanced_testcase {
                 report_customsql_get_month_starts(strtotime('23:59 29 November 2009')));
     }
 
-    /**
-     * Test element type detection.
-     * @covers ::report_customsql_get_element_type
-     */
     public function test_report_customsql_get_element_type(): void {
         $this->assertEquals('date_time_selector', report_customsql_get_element_type('start_date'));
         $this->assertEquals('date_time_selector', report_customsql_get_element_type('startdate'));
@@ -131,20 +122,12 @@ class report_test extends \advanced_testcase {
         $this->assertEquals('text', report_customsql_get_element_type('mandated'));
     }
 
-    /**
-     * Test token substitution.
-     * @covers ::report_customsql_substitute_user_token
-     */
     public function test_report_customsql_substitute_user_token(): void {
         $this->assertEquals('SELECT COUNT(*) FROM oh_quiz_attempts WHERE user = 123',
                 report_customsql_substitute_user_token('SELECT COUNT(*) FROM oh_quiz_attempts '.
                         'WHERE user = %%USERID%%', 123));
     }
 
-    /**
-     * Test capability options.
-     * @covers ::report_customsql_capability_options
-     */
     public function test_report_customsql_capability_options(): void {
         $capoptions = [
             'report/customsql:view' => get_string('anyonewhocanveiwthisreport', 'report_customsql'),
@@ -155,10 +138,6 @@ class report_test extends \advanced_testcase {
 
     }
 
-    /**
-     * Test runable options.
-     * @covers ::report_customsql_runable_options
-     */
     public function test_report_customsql_runable_options(): void {
         $options = [
             'manual'  => get_string('manual', 'report_customsql'),
@@ -170,10 +149,6 @@ class report_test extends \advanced_testcase {
         $this->assertEquals($options, report_customsql_runable_options());
     }
 
-    /**
-     * Test daily run options.
-     * @covers ::report_customsql_daily_at_options
-     */
     public function test_report_customsql_daily_at_options(): void {
         $time = [];
         for ($h = 0; $h < 24; $h++) {
@@ -183,10 +158,6 @@ class report_test extends \advanced_testcase {
         $this->assertEquals($time, report_customsql_daily_at_options());
     }
 
-    /**
-     * Test email options.
-     * @covers ::report_customsql_email_options
-     */
     public function test_report_customsql_email_options(): void {
         $options = [
             'emailnumberofrows' => get_string('emailnumberofrows', 'report_customsql'),
@@ -195,28 +166,16 @@ class report_test extends \advanced_testcase {
         $this->assertEquals($options, report_customsql_email_options());
     }
 
-    /**
-     * Test bad words list.
-     * @covers ::report_customsql_bad_words_list
-     */
     public function test_report_customsql_bad_words_list(): void {
         $options = ['ALTER', 'CREATE', 'DELETE', 'DROP', 'GRANT', 'INSERT', 'INTO', 'TRUNCATE', 'UPDATE'];
         $this->assertEquals($options, report_customsql_bad_words_list());
     }
 
-    /**
-     * Test bad words.
-     * @covers ::report_customsql_contains_bad_word
-     * */
     public function test_report_customsql_contains_bad_word(): void {
         $string = 'DELETE * FROM prefix_user u WHERE u.id  > 0';
         $this->assertEquals(1, report_customsql_contains_bad_word($string));
     }
 
-    /**
-     * Test daily reports.
-     * @covers ::report_customsql_get_daily_time_starts
-     */
     public function test_report_customsql_get_ready_to_run_daily_reports(): void {
         global $DB;
         $this->resetAfterTest(true);
@@ -290,10 +249,6 @@ class report_test extends \advanced_testcase {
         $this->assertTrue(report_customsql_is_daily_report_ready($report, $timenow));
     }
 
-    /**
-     * Test integer detection.
-     * @covers ::report_customsql_is_integer
-     */
     public function test_report_customsql_is_integer(): void {
         $this->assertTrue(report_customsql_is_integer(1));
         $this->assertTrue(report_customsql_is_integer('1'));
@@ -301,10 +256,6 @@ class report_test extends \advanced_testcase {
         $this->assertFalse(report_customsql_is_integer('2013-10-07'));
     }
 
-    /**
-     * Test table headers.
-     * @covers ::report_customsql_get_table_headers
-     */
     public function test_report_customsql_get_table_headers(): void {
         $rawheaders = [
                 'String date',
@@ -331,10 +282,6 @@ class report_test extends \advanced_testcase {
         $this->assertEquals([3 => 4, 4 => -1, 5 => 7, 7 => -1], $linkcolumns);
     }
 
-    /**
-     * Test column names.
-     * @covers ::report_customsql_pretify_column_names
-     */
     public function test_report_customsql_pretify_column_names(): void {
         $row = new \stdClass();
         $row->column = 1;
@@ -345,10 +292,6 @@ class report_test extends \advanced_testcase {
                 report_customsql_pretify_column_names($row, $query));
     }
 
-    /**
-     * Test column multi-line names.
-     * @covers ::report_customsql_pretify_column_names
-     */
     public function test_report_customsql_pretify_column_names_multi_line(): void {
         $row = new \stdClass();
         $row->column = 1;
@@ -363,10 +306,6 @@ class report_test extends \advanced_testcase {
                 report_customsql_pretify_column_names($row, $query));
     }
 
-    /**
-     * Test pretty column names.
-     * @covers ::report_customsql_pretify_column_names
-     */
     public function test_report_customsql_pretify_column_names_same_name_diff_capitialisation(): void {
         $row = new \stdClass();
         $row->course = 'B747-19B';
@@ -377,10 +316,6 @@ class report_test extends \advanced_testcase {
 
     }
 
-    /**
-     * Test pretty column names.
-     * @covers ::report_customsql_pretify_column_names
-     */
     public function test_report_customsql_pretify_column_names_issue(): void {
         $row = new \stdClass();
         $row->website = 'B747-19B';
@@ -411,10 +346,6 @@ class report_test extends \advanced_testcase {
 
     }
 
-    /**
-     * Test row display.
-     * @covers ::report_customsql_display_row
-     */
     public function test_report_customsql_display_row(): void {
         $rawdata = [
                 'Not a date',
@@ -439,11 +370,6 @@ class report_test extends \advanced_testcase {
                 '&lt;b&gt;Raw HTML&lt;/b&gt;'], report_customsql_display_row($rawdata, $linkcolumns));
     }
 
-    /**
-     * Test plugin emailing of reports
-     *
-     * @covers ::report_customsql_email_report
-     */
     public function test_report_customsql_email_report(): void {
         global $CFG, $DB;
 
@@ -507,11 +433,6 @@ class report_test extends \advanced_testcase {
         $sink->close();
     }
 
-    /**
-     * Test plugin downloading of reports.
-     *
-     * @covers ::report_customsql_downloadurl
-     */
     public function test_report_custom_sql_download_report_url(): void {
         global $DB;
 
@@ -547,10 +468,6 @@ class report_test extends \advanced_testcase {
         $this->assertEquals($expected, $url->out(false));
     }
 
-    /**
-     * Test writing a CSV row.
-     * @covers ::report_customsql_write_csv_row
-     */
     public function test_report_customsql_write_csv_row(): void {
         global $CFG;
         $this->resetAfterTest();
